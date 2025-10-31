a8ad66f7698afb90a88bf5a4e31c8868091acd77
arokah25 <adamrokah96@gmail.com>
2025-10-31 16:07:08 +0100

    fixed factory

--- src/moe/heads/factory.py
+++ src/moe/heads/factory.py
@@ -3,10 +3,10 @@ from .soft_moe import SoftMoEHead
 from .sparse_moe import SparseMoEHead
 # (we'll add Sparse/Hard imports later)
 
-def build_head(kind: str, **kw): #kw=keyword arguments
+def build_head(kind: str, **kwargs): #kwargs=keyword arguments
     k = kind.lower()
     if k == "dense":
-        return DenseHead(kw["in_dim"], kw["width"], kw["num_classes"])
+        return DenseHead(kwargs["in_dim"], kwargs["width"], kwargs["num_classes"])
     if k == "softmoe":
         return SoftMoEHead(kwargs["in_dim"], kwargs["num_classes"], kwargs["num_experts"], kwargs["hidden_mult"], kwargs["temperature"], kwargs["dropout_p"])
     if k == "sparsemoe":
